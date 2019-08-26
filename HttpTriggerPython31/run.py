40d02874e235da74afa864d71b5984c80528ffd1
Raymond Zheng <raymong.zheng@health.telstra.com>
2019-08-26 17:39:33 +1000

    u2

--- HttpTriggerPython31/run.py
+++ HttpTriggerPython31/run.py
@@ -5,7 +5,7 @@ import platform
 
 postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-c=p.ClamdNetworkSocket(host='52.189.236.53')
+c=p.ClamdNetworkSocket(host='10.0.0.4')
 res=c.scan_stream('')
 response.write(postreqdata)
 response.close()
